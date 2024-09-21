824f0e8fbddb30e74f62dae0d1f31d851acc72be
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-09-21 08:02:53 +0300

    finish modificatio of about  legal services page

--- app/legal-services/page.jsx
+++ app/legal-services/page.jsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 
 import Accordion from "@/components/legal-services/client/Accordion";
 import LegalServicesHero from "@/components/legal-services/legalServicesHero/LegalServicesHero";
-import LegalServicesSuccessFactor from "@/components/legal-services/legalServicesSuccessFactor/legalServicesSuccessFactor";
+import LegalServicesSuccessFactor from "@/components/legal-services/legalServicesSuccessFactor/LegalServicesSuccessFactor";
 import LegalServiceOurOfferings from "@/components/legal-services/legalServiceOurOfferings/LegalServiceOurOfferings";
 import LegalServicesSectors from "@/components/legal-services/legalServicesSectorAndPractices/LegalServicesSectors";
 import LegalServicesPractices from "@/components/legal-services/legalServicesSectorAndPractices/LegalServicesPractices";
