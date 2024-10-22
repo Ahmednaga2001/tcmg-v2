88546f09bad40a7d6ecaf4effc835daf83b6321d
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-10-23 02:08:27 +0300

    finsih some mod in branches page

--- app/offices/page.jsx
+++ app/offices/page.jsx
@@ -7,7 +7,7 @@ import BranchContactForm from "@/components/branches/client/branchContactForm/Br
 import BranchLocationsMob from "@/components/branches/BranchLocations/BranchLocationsMob"
 import ContactMobile from "@/components/branches/ContactMobile/ContactMobile";
 export const metadata = {
-  title: "Branches",
+  title: "Offices",
   description : "هل تبحث عن عناوين وفروع مكاتب محامين أو مكاتب محاماة في القاهرة؟ لا داعى للبحث أكثر، نحن بجانبك من خلال فروعنا الموجودة فى مصر ودول الوطن العربي.",
   keywords : "فروع مكاتب TCMG، مكاتب محاماة دولية، فروع مكاتب المحامين"
 };
